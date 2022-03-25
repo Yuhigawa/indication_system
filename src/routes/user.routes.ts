7ed4520581d8dcbefcb73dc1494d70cf56171fc0
yuhigawa <pablostreiski@outlook.com>
2022-03-25 18:29:12 -0300

    helper routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,15 @@
 import { Request, Response, Router } from "express";
-import { createIndication, increaseIndication, searchUserRoute, isUserCreated } from '../controller/user.controller';
+import { increaseIndication, searchUserRoute, isUserCreated } from '../controller/user.controller';
+
+import { getAllIps } from '../controller/ip.controller';
+
 const router = Router();
 
 router.post('/:id', isUserCreated);
-router.post('/indication/:id', increaseIndication);
-router.get('/indication/:id', searchUserRoute);
+router.get('/indication/:hashkey', increaseIndication);
+
+// TODO: helper routes will get deleted in production
+router.post('/indication/:id', searchUserRoute);
+router.get('/ips', getAllIps);
 
 export default router;
