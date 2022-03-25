4cf82123fde1e59fc74b1d5566b7057f680562ed
Yuhigawa <pablostreiski@outlook.com>
2022-03-25 10:32:36 -0300

    created auxiliar route for testing activecampaign

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index";
+import userIndication from "./routes/user.routes";
 
 const app = express();
 
@@ -12,6 +13,15 @@ app.use( express.urlencoded({ extended: true }) );
 app.use( express.json() );
 app.use( express.json({ type: "application/vnd.api+json" }) );
 app.use( router );
+app.use( userIndication );
+
+import { getUserById } from "./controller/service/activecampaign";
+
+app.get('/test/:id', async (req, res) => {
+    let response = await getUserById(parseInt(req.params.id));
+
+    res.status(response['status']).send(response['data']);
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening to http://localhost:${PORT}`))
