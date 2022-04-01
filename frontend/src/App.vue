fb35aca54f8b426a49fa788189c80d8bc4528b04
yuhigawa <pablostreiski@outlook.com>
2022-04-01 13:37:05 -0300

    FRONT END: initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Indication from './components/Indication.vue'
+</script>
+
+<template>
+  <Indication />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
---
+++ frontend/src/components/Indication.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component ({
+  name: 'Indication'
+})
+export default class Indication extends Vue {
+  private msg: string = "welcome to my app"
+}
+</script>
+
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
+
+<style scoped>
+</style>
